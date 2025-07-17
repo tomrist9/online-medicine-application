c6c634510668592ff20188fc41546c79246017d0
Tomris Teymurlu <tomristt9@gmail.com>
2025-07-17 13:56:14 +0400

    - Added CreditEntryEntity for JPA mapping - Created CreditEntryDataAccessMapper for entity-domain conversions - Implemented CreditEntryRepositoryImpl as adapter for domain repository

---
+++ payment-service/payment-dataaccess/src/main/java/com/online/medicine/application/creditentry/adapter/CreditEntryRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.online.medicine.application.creditentry.adapter;
+
+import com.online.medicine.application.creditentry.mapper.CreditEntryDataAccessMapper;
+import com.online.medicine.application.creditentry.repository.CreditEntryJpaRepository;
+import com.online.medicine.application.entity.CreditEntry;
+import com.online.medicine.application.order.service.domain.valueobject.CustomerId;
+import com.online.medicine.application.payment.service.domain.ports.output.repository.CreditEntryRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class CreditEntryRepositoryImpl implements CreditEntryRepository {
+
+    private final CreditEntryJpaRepository creditEntryJpaRepository;
+    private final CreditEntryDataAccessMapper creditEntryDataAccessMapper;;
+
+    public CreditEntryRepositoryImpl(CreditEntryJpaRepository creditEntryJpaRepository,
+                                     CreditEntryDataAccessMapper creditEntryDataAccessMapper) {
+        this.creditEntryJpaRepository = creditEntryJpaRepository;
+        this.creditEntryDataAccessMapper = creditEntryDataAccessMapper;
+    }
+
+    @Override
+    public CreditEntry save(CreditEntry creditEntry) {
+        return creditEntryDataAccessMapper
+                .creditEntryEntityToCreditEntry(creditEntryJpaRepository
+                        .save(creditEntryDataAccessMapper.creditEntryToCreditEntryEntity(creditEntry)));
+    }
+
+    @Override
+    public Optional<CreditEntry> findByCustomerId(CustomerId customerId) {
+        return creditEntryJpaRepository
+                .findByCustomerId(customerId.getValue())
+                .map(creditEntryDataAccessMapper::creditEntryEntityToCreditEntry);
+    }
+}
---
+++ payment-service/payment-dataaccess/src/main/java/com/online/medicine/application/creditentry/exception/CreditEntryDataaccessException.java
@@ -0,0 +1,8 @@
+package com.online.medicine.application.creditentry.exception;
+
+public class CreditEntryDataaccessException extends RuntimeException {
+
+    public CreditEntryDataaccessException(String message) {
+        super(message);
+    }
+}
