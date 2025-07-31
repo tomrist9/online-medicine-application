f97208b4ab366609f5de7e2534d682932d4beeba
Tomris Teymurlu <tomristt9@gmail.com>
2025-07-31 22:24:46 +0400

    implement OrderCreateCommandHandler class

--- order-service/order-domain/order-application-service/src/main/java/com/online/medicine/application/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/online/medicine/application/order/service/domain/OrderCreateCommandHandler.java
@@ -4,10 +4,8 @@ import com.online.medicine.application.order.service.domain.dto.create.CreateOrd
 import com.online.medicine.application.order.service.domain.dto.create.CreateOrderResponse;
 import com.online.medicine.application.order.service.domain.mapper.OrderDataMapper;
 
-import com.online.medicine.application.order.service.domain.ports.output.repository.CustomerRepository;
-import com.online.medicine.application.order.service.domain.ports.output.repository.OrderRepository;
-import com.online.medicine.application.order.service.domain.ports.output.repository.PharmacyRepository;
-import com.online.medicine.domain.order.service.domain.OrderDomainService;
+import com.online.medicine.application.order.service.domain.outbox.scheduler.payment.PaymentOutboxHelper;
+import com.online.medicine.application.outbox.OutboxStatus;
 import com.online.medicine.domain.order.service.domain.entity.Customer;
 import com.online.medicine.domain.order.service.domain.entity.Order;
 import com.online.medicine.domain.order.service.domain.entity.Pharmacy;
@@ -27,71 +25,40 @@ import java.util.UUID;
 @Component
 public class OrderCreateCommandHandler {
 
-//    private final OrderCreateHelper orderCreateHelper;
+
+    private final OrderCreateHelper orderCreateHelper;
     private final OrderDataMapper orderDataMapper;
-    private final OrderDomainService orderDomainService;
-    private final PharmacyRepository pharmacyRepository;
-    private final CustomerRepository customerRepository;
-    private final OrderRepository orderRepository;
 
-    public OrderCreateCommandHandler(OrderDataMapper orderDataMapper, OrderDomainService orderDomainService, PharmacyRepository pharmacyRepository, CustomerRepository customerRepository, OrderRepository orderRepository) {
+    private final PaymentOutboxHelper paymentOutboxHelper;
+    private final OrderSagaHelper orderSagaHelper;
+
+    public OrderCreateCommandHandler(OrderCreateHelper orderCreateHelper,
+                                     OrderDataMapper orderDataMapper,
+                                     PaymentOutboxHelper paymentOutboxHelper,
+                                     OrderSagaHelper orderSagaHelper) {
+        this.orderCreateHelper = orderCreateHelper;
         this.orderDataMapper = orderDataMapper;
-        this.orderDomainService = orderDomainService;
-        this.pharmacyRepository = pharmacyRepository;
-        this.customerRepository = customerRepository;
-        this.orderRepository = orderRepository;
+        this.paymentOutboxHelper = paymentOutboxHelper;
+        this.orderSagaHelper = orderSagaHelper;
     }
-//    private final PaymentOutboxHelper paymentOutboxHelper;
-//    private final OrderSagaHelper orderSagaHelper;
 
-//    public OrderCreateCommandHandler(OrderCreateHelper orderCreateHelper,
-//                                     OrderDataMapper orderDataMapper,
-//                                     PaymentOutboxHelper paymentOutboxHelper,
-//                                     OrderSagaHelper orderSagaHelper) {
-//        this.orderCreateHelper = orderCreateHelper;
-//        this.orderDataMapper = orderDataMapper;
-//        this.paymentOutboxHelper = paymentOutboxHelper;
-//        this.orderSagaHelper = orderSagaHelper;
-//    }
-//
+
     @Transactional
     public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand) {
-        checkCustomer(createOrderCommand.getCustomerId());
-        Pharmacy pharmacy=checkPharmacy(createOrderCommand);
-        Order order=orderDataMapper.createOrderCommandToOrder(createOrderCommand);
-        OrderCreatedEvent orderCreatedEvent = orderDomainService.validateAndInitiateOrder(order, pharmacy);
-        Order orderResult=saveOrder(order);
-        log.info("Order is created with id: {}", orderResult.getId().getValue());
+       OrderCreatedEvent orderCreatedEvent = orderCreateHelper.persistOrder(createOrderCommand);
+       log.info("Order is created with id: {}", orderCreatedEvent.getOrder().getId().getValue());
+       CreateOrderResponse createOrderResponse = orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder(),
+               "Order created successfully " );
 
-        return orderDataMapper.orderToCreateOrderResponse(orderResult,"Order created successfully");
+       paymentOutboxHelper.savePaymentOutboxMessage(orderDataMapper
+               .orderCreatedEventToOrderPaymentEventPayload(orderCreatedEvent),
+               orderCreatedEvent.getOrder().getOrderStatus(),
+               orderSagaHelper.orderStatusToSagaStatus(orderCreatedEvent.getOrder().getOrderStatus()),
+               OutboxStatus.STARTED,
+               UUID.randomUUID());
+
+       log.info("Returning CreateOrderResponse with order id {}", orderCreatedEvent.getOrder().getId());
+
+       return createOrderResponse;
     }
-
-    private void checkCustomer(UUID customerId) {
-        Optional<Customer> customer = customerRepository.findCustomer(customerId);
-        if (customer.isEmpty()) {
-            log.warn("Could not find customer with customer id: {}", customerId);
-            throw new OrderDomainException("Could not find customer with customer id: " + customerId);
-        }
-    }
-
-    private Order saveOrder(Order order) {
-
-        Order savedOrder = orderRepository.save(order);
-        log.info("Order saved with id: {}", savedOrder.getId().getValue());
-        return savedOrder;
-    }
-
-    private Pharmacy checkPharmacy(CreateOrderCommand createOrderCommand) {
-        Pharmacy pharmacy=orderDataMapper.createOrderCommandToPharmacy(createOrderCommand);
-        Optional<Pharmacy> optionalPharmacy=pharmacyRepository.findPharmacyInformation(pharmacy);
-        if(optionalPharmacy.isEmpty()){
-            log.warn("Could not find pharmacy with pharmacy id: {}", createOrderCommand.getPharmacyId());
-            throw new OrderDomainException("Could not find pharmacy with pharmacy id: " + createOrderCommand.getPharmacyId());
-        }
-        return optionalPharmacy.get();
-    }
-
-
-
-
 }
