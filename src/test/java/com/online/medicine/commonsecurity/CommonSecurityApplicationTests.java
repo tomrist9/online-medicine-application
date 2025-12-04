305cecd58b71233d12b455c654dfcbd0fa4b486c
tomrist9 <tomristt9@gmail.com>
2025-12-04 15:28:11 +0400

    add common security module

---
+++ src/test/java/com/online/medicine/commonsecurity/CommonSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.medicine.commonsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
